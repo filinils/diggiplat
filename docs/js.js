4c92393bba86b40a9cbb927dcd821f70a9535106
Henrik Schulze Nilsson <henrikn86@gmail.com>
2017-08-08 15:08:44 +0200

    Added Annie Löv functionality

--- docs/js.js
+++ docs/js.js
@@ -1,3 +1,15 @@
-(function(){
-    document.getElementById('first').innerText = 'Hello';
-})(); 
+(function () {
+    console.log("hej");
+    var timings = [200, 400, 600, 800, 1000, 1200, 1400];
+    var lyric = ['Annie', '', 'Annie', '', 'Annie', '', 'Löööv'];
+    for (var i = 0; i < timings.length; i++) {
+        (function (index) {
+            setTimeout(writeLyric, timings[index], index);
+        })(i)
+    }
+
+    function writeLyric(index) {
+        console.log(lyric[index]);
+        document.getElementById('first').innerText = lyric[index];
+    }
+})();
